2a6f651dee1a86595ae36da776db470af455a286
fpoblete <fpoblete@vm-0.fpoblete.koding.kd.io>
2014-08-24 02:53:01 +0000

    We log to google using ENV variables, this way is not necessary to commit the secret keys

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,10 @@
-#TODO GOOGLE CLIENT ID and GOOGLE_CLIENT_SECRET should be environment variables and they shouldn't be comitted
+# the variables ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_CLIENT_SECRET"] must be set up on an initializer file like this:
+#        ENV['GOOGLE_CLIENT_ID'] = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+#        ENV['GOOGLE_CLIENT_SECRET'] = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+
+
 Rails.application.config.middleware.use OmniAuth::Builder do
-	#provider :google_oauth2, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_CLIENT_SECRET"]
-	provider :google_oauth2, '616679661881-pnn0tseh0q672jg90ophcqis5lb8rdt0.apps.googleusercontent.com', 'FCHSjeGbFNKXEey2D6yi_Yin', {
+	provider :google_oauth2, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_CLIENT_SECRET"], {
 		scope: 'https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/calendar',
 		redirect_uri:'http://fpoblete.kd.io:3000/auth/google_oauth2/callback'
 	}
