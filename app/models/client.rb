744392f8c53f2ed2a8362de95eb7462c1d0ae73f
fipa <fernandopobletearrau@gmail.com>
2014-11-24 01:28:55 +0000

    Total ammount is calculated considering the length of each date

--- app/models/client.rb
+++ app/models/client.rb
@@ -6,7 +6,10 @@ class Client < ActiveRecord::Base
 	def dates(date_min = nil, date_max = nil, max_results = 15)
 		dates = Hash.new
 		dates[:entries] = self.calendar.get_dates_from_client_between_dates(self, date_min, date_max)
-		dates[:total_ammount] = dates[:entries].size * self.ammount
+		dates[:total_ammount] = 0
+		dates[:entries].each do |entry|
+			dates[:total_ammount]+= ((entry.ending - entry.begining)/3600).round * self.ammount
+		end
 		return dates
 	end
 	
