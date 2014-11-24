81eacdfa0e82c5397fa915afd78e6e48fe07a367
fipa <fernandopobletearrau@gmail.com>
2014-11-24 01:09:31 +0000

    It applies the block and void filters to the events

--- app/models/calendar.rb
+++ app/models/calendar.rb
@@ -9,13 +9,12 @@ class Calendar < ActiveRecord::Base
 		google_client.authorization.access_token = self.user.current_token
 		calendar_service = google_client.discovered_api('calendar', 'v3')
 
-		
-
+		#TODO averiguar lo del -3 y corregirlo
 		parameters = Hash.new
 		parameters[:calendarId] = self.calendar_key
 		parameters[:timeMin] = date_min + "T00:00:00-03:00"
-		parameters[:timeMax] = date_max + "T00:00:00-03:00"
-		parameters[:fields] = 'items(created,description,end,endTimeUnspecified,id,start,summary)'
+		parameters[:timeMax] = date_max + "T23:59:59-03:00"
+		parameters[:fields] = 'items(created,description,end,id,start,summary)'
 		parameters[:singleEvents] = true
 		parameters[:q] = client.full_name
 		parameters[:orderBy] = "startTime"
@@ -28,20 +27,48 @@ class Calendar < ActiveRecord::Base
 			:headers => {'Content-Type' => 'application/json'}
 		)
 
-		logger.info("respuesta de la API " + results.to_s)
+		logger.info("respuesta de la API " + results.data.items.to_s)
 
 		#TODO manejar respuesta de invalid credentials
+
+		#TODO averiguar lo del -3 y corregirlo
 		parsed_results = Array.new
 		results.data.items.each do |item|
-		date = CalendarDate.new
-		date.summary = item.summary
-		date.begining = item.start.dateTime
-		date.ending = item.end.dateTime
-		parsed_results << date
-
+			date = CalendarDate.new
+			date.summary = item.summary
+			date.begining = item.start.dateTime.advance(:hours => -3)
+			date.ending = item.end.dateTime.advance(:hours => -3)
+			parsed_results << date unless Calendar.filters_apply?(date, client)
 		end
 		logger.info("resultados de la API " + parsed_results.to_s)
 		return parsed_results
 
 	end
+
+	private
+
+
+	def self.filters_apply?(date, client)
+		if Calendar.block_filter_applies(date.summary)
+			logger.info("filtro aplica")
+			return true
+		elsif Calendar.voided_event_filter_applies(date.summary, client.full_name)
+			logger.info("filtro aplica")
+			return true
+		else
+			return false
+		end
+	end
+
+	def self.block_filter_applies(event_summary)
+		logger.info("verificando filtro BLOQUE para [#{event_summary}]")
+		return /BLOQUE/.match(event_summary)
+	end
+
+	def self.voided_event_filter_applies(event_summary, client_name)
+		logger.info("verificando filtro voided para [#{event_summary}],[#{client_name}]")
+		return /\(#{client_name}\)/.match("#{event_summary}")
+	end
+
+
 end
